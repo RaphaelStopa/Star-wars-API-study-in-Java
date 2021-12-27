be81ddd937c165f9773e3f472e13e9f834500765
RaphaelEsparta <83084343+RaphaelEsparta@users.noreply.github.com>
2021-12-27 10:18:53 -0300

    no message

--- src/main/java/com/example/demo/facade/mapper/KnownMastersMapper.java
+++ src/main/java/com/example/demo/facade/mapper/KnownMastersMapper.java
@@ -4,7 +4,7 @@ import com.example.demo.domain.KnownMaster;
 import com.example.demo.facade.dto.KnownMasterDTO;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring", uses = {})
+@Mapper(componentModel = "spring", uses = {ForceUserMapper.class})
 public interface KnownMastersMapper extends EntityMapper<KnownMasterDTO, KnownMaster> {
 
     KnownMasterDTO toDto(KnownMaster knownMaster);
